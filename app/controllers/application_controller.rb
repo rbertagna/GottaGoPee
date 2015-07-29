1e7d150c6538e7cbbb1870f1af104c2e7a16be0c
jazus97 <jazus97@gmail.com>
2015-07-29 16:33:48 +0000

    whatver

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,20 @@ require_relative "../models/bathroom.rb"
 
 class ApplicationController < Sinatra::Base
   
+  def index
+    if params[:bathroom].present?
+      @bathrooms = Bathroom.near(params[:search], 1, :order => :distance)
+    else
+      @bathrooms = Bathroom.all
+    end
+  end
+  
+  post '/map' do
+    "Hello World"
+    erb :show
+  end
+  
+  
   set :views, "app/views"
   set :public, "public"
   
